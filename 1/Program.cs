e5abd2910ff54813158aecee56369b818a40daea
naduhawww <117995427+naduhawww@users.noreply.github.com>
2022-11-23 22:08:34 +0700

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int result = (num / 10) % 10;
+Console.WriteLine(result);
---
+++ 2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Берем числа до 100000
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine()!;
+
+if(number.Length ! < 3)
+{
+    Console.WriteLine("В этом числе третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра равна {number[2]}");
+}
